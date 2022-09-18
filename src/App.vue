cdeac478d0390857f80948e024f99a7cbe541d8b
chaochaooo <373930051@qq.com>
2022-09-18 21:26:41 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1 class="test">hello</h1>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.test {
+  @apply text-red-800;
+}
+</style>
---
+++ vite.config.js
@@ -0,0 +1,31 @@
+import { resolve } from 'path';
+
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import ViteComponents from 'vite-plugin-components';
+
+import ViteIcons, { ViteIconsResolver } from 'vite-plugin-icons';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src')
+    }
+  },
+  plugins: [
+    vue(),
+    ViteComponents({
+      extensions: ['vue'],
+      // auto import icons
+      customComponentResolvers: [
+        // https://github.com/antfu/vite-plugin-icons
+        ViteIconsResolver({
+          componentPrefix: ''
+          // enabledCollections: ['carbon']
+        })
+      ]
+    }),
+    ViteIcons()
+  ]
+});
